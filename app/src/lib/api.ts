a7b855da1d420aee5a95c31edda5e26795ca84f5
Antoine TORRES <antoine.t@paxcia.net>
2026-02-24 17:12:47 +0100

    gg

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -1,3 +1,6 @@
 export function getApiBaseUrl() {
+  if (typeof window !== "undefined") {
+    return process.env.NEXT_PUBLIC_API_BASE_URL || window.location.origin;
+  }
   return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
 }
